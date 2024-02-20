bb990808e3982e80f20aedef806cec72eb9b2d02
tuanhungvu <tuanhungvu@gmail.com>
2024-02-20 13:59:45 +0100

    clean code

--- world_model/networks/gpt2.py
+++ world_model/networks/gpt2.py
@@ -114,12 +114,12 @@ class Block(nn.Module):
 @dataclass
 class GPTConfig:
     block_size: int = 8                 # maximum sequence length
-    vocab_size: int = 1104
-    nb_tokens_per_timestep: int  = 352  # nb_tokens_per_timestep = hw + act_sz
+    vocab_size: int = 1104              # vocabulary size = visual_vocab_size + sum(action_vocab_sizes) (e.g. 1104 = 1024 + 50 + 30)
+    nb_tokens_per_timestep: int  = 352  # nb_tokens_per_timestep = #visual-tokens + #action-tokens (e.g. 14*25 + 2)
     n_layer: int = 12                   # number of transformer blocks
     n_head: int = 16                    # number of attention heads
     n_embd: int = 256                   # embedding dimension for the world model    
-    dropout: float = 0.15                # dropout rate
+    dropout: float = 0.15               # dropout rate
     bias: bool = True                   # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
 class GPT2_Core(nn.Module):
@@ -202,7 +202,7 @@ class GPT2_Core(nn.Module):
         x = self.transformer.drop(emb_in)
         for block in self.transformer.h:
             x = block(x)
-        emb_out = self.transformer.ln_f(x)                                  # (b x t*(hw+act_sz) x n_embd)
+        emb_out = self.transformer.ln_f(x)                                                  # (b x t*(hw+act_sz) x n_embd)
         
         if not inference:
             img_logits = self.lm_head(emb_out)
@@ -210,177 +210,22 @@ class GPT2_Core(nn.Module):
             img_logits = self.lm_head(emb_out[:, [-1], :]) # note: using list [-1] to preserve the time dim
         return img_logits
 
-    def crop_block_size(self, block_size):
-        # model surgery to decrease the block size if necessary
-        # e.g. we may load the GPT2 pretrained model checkpoint (block size 1024)
-        # but want to use a smaller block size for some smaller, simpler model
-        assert block_size <= self.config.block_size
-        self.config.block_size = block_size
-        self.transformer.wte.weight = nn.Parameter(self.transformer.wte.weight[:block_size])
-        for block in self.transformer.h:
-            if hasattr(block.attn, 'bias'):
-                block.attn.bias = block.attn.bias[:,:,:block_size,:block_size]
-
-    @classmethod
-    def from_pretrained(cls, model_type, override_args=None):
-        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
-        override_args = override_args or {} # default to empty dict
-        # only dropout can be overridden see more notes below
-        assert all(k == 'dropout' for k in override_args)
-        from transformers import GPT2LMHeadModel
-        print("loading weights from pretrained gpt: %s" % model_type)
-
-        # n_layer, n_head and n_embd are determined from model_type
-        config_args = {
-            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
-            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
-            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
-            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
-        }[model_type]
-        print("forcing vocab_size=50257, block_size=1024, bias=True")
-        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
-        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
-        config_args['bias'] = True # always True for GPT model checkpoints
-        # we can override the dropout rate, if desired
-        if 'dropout' in override_args:
-            print(f"overriding dropout rate to {override_args['dropout']}")
-            config_args['dropout'] = override_args['dropout']
-        # create a from-scratch initialized minGPT model
-        config = GPTConfig(**config_args)
-        model = GPT(config)
-        sd = model.state_dict()
-        sd_keys = sd.keys()
-        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
-
-        # init a huggingface/transformers model
-        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
-        sd_hf = model_hf.state_dict()
-
-        # copy while ensuring all of the parameters are aligned and match in names and shapes
-        sd_keys_hf = sd_hf.keys()
-        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
-        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
-        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
-        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
-        # this means that we have to transpose these weights when we import them
-        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
-        for k in sd_keys_hf:
-            if any(k.endswith(w) for w in transposed):
-                # special treatment for the Conv1D weights we need to transpose
-                assert sd_hf[k].shape[::-1] == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k].t())
-            else:
-                # vanilla copy over the other parameters
-                assert sd_hf[k].shape == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k])
-
-        return model
-
-    def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
-        # start with all of the candidate parameters
-        param_dict = {pn: p for pn, p in self.named_parameters()}
-        # filter out those that do not require grad
-        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
-        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
-        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
-        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
-        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
-        optim_groups = [
-            {'params': decay_params, 'weight_decay': weight_decay},
-            {'params': nodecay_params, 'weight_decay': 0.0}
-        ]
-        num_decay_params = sum(p.numel() for p in decay_params)
-        num_nodecay_params = sum(p.numel() for p in nodecay_params)
-        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
-        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
-        # Create AdamW optimizer and use the fused version if it is available
-        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
-        use_fused = fused_available and device_type == 'cuda'
-        extra_args = dict(fused=True) if use_fused else dict()
-        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
-        print(f"using fused AdamW: {use_fused}")
-
-        return optimizer
-
-    def estimate_mfu(self, fwdbwd_per_iter, dt):
-        """ estimate model flops utilization (MFU) in units of A100 bfloat16 peak FLOPS """
-        # first estimate the number of flops we do per iteration.
-        # see PaLM paper Appendix B as ref: https://arxiv.org/abs/2204.02311
-        N = self.get_num_params()
-        cfg = self.config
-        L, H, Q, T = cfg.n_layer, cfg.n_head, cfg.n_embd//cfg.n_head, cfg.block_size
-        flops_per_token = 6*N + 12*L*H*Q*T
-        flops_per_fwdbwd = flops_per_token * T
-        flops_per_iter = flops_per_fwdbwd * fwdbwd_per_iter
-        # express our flops throughput as ratio of A100 bfloat16 peak flops
-        flops_achieved = flops_per_iter * (1.0/dt) # per second
-        flops_promised = 312e12 # A100 GPU bfloat16 peak flops is 312 TFLOPS
-        mfu = flops_achieved / flops_promised
-        return mfu
-
-    @torch.no_grad()
-    def generate(self, img, act, max_new_tokens, temperature=1.0, top_k=None):
-        """
-        Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
-        the sequence max_new_tokens times, feeding the predictions back into the model each time.
-        Most likely you'll want to make sure to be in model.eval() mode of operation for this.
-        """
-        for _ in range(max_new_tokens):
-            # if the sequence context is growing too long we must crop it at block_size
-            img_cond = img if img.size(1) <= self.config.block_size else img[:, -self.config.block_size:]
-            act_cond = act if act.size(1) <= self.config.block_size else act[:, -self.config.block_size:]
-            # forward the model to get the logits for the index in the sequence
-            img_logits, act_logits, _ = self(img_cond, act_cond)
-            # pluck the logits at the final step and scale by desired temperature
-            img_logits = img_logits[:, -1, :] / temperature
-            act_logits = act_logits[:, -1, :] / temperature
-            # optionally crop the logits to only the top k options
-            # if top_k is not None:
-            #     v, _ = torch.topk(img_logits, min(top_k, img_logits.size(-1)))
-            #     img_logits[img_logits < v[:, [-1]]] = -float('Inf')
-            # apply softmax to convert logits to (normalized) probabilities
-            img_probs = F.softmax(img_logits, dim=-1)
-            # sample from the distribution
-            # reshape probs to remove the batch dimension
-            bz, tok_sz, vocab_sz = img_probs.size()
-            img_probs = img_probs.view(-1, vocab_sz)
-            img_next = torch.multinomial(img_probs, num_samples=1)
-            # put back the batch dimension
-            img_next = img_next.view(bz, 1, tok_sz)
-            
-            act_probs = F.softmax(act_logits, dim=-1)
-            # sample from the distribution
-            # reshape probs to remove the batch dimension
-            bz, tok_sz, vocab_sz = act_probs.size()
-            act_probs = act_probs.view(-1, vocab_sz)
-            act_next = torch.multinomial(act_probs, num_samples=1)
-            # put back the batch dimension
-            act_next = act_next.view(bz, 1, tok_sz)
-            
-            # append sampled index to the running sequence and continue
-            img = torch.cat((img, img_next), dim=1)
-            act = torch.cat((act, act_next), dim=1)
-
-        return img
-
 class GPT2(nn.Module):
     
     def __init__(
         self, 
-        embedding_dim,
-        num_heads,
-        num_layers, 
-        vocabulary_size,    
-        nb_timesteps, # not used in DummyGPT
-        nb_tokens_per_timestep, # not used in DummyGPT
-        dropout_rate = 0.15,
-        bias = True
+        embedding_dim,                  # embedding dimension for the world model
+        num_heads,                      # number of attention heads
+        num_layers,                     # number of transformer blocks
+        vocabulary_size,                # vocabulary size = visual_vocab_size + sum(action_vocab_sizes)
+        nb_timesteps,                   # maximum sequence length
+        nb_tokens_per_timestep,         # nb_tokens_per_timestep = #visual-tokens + #action-tokens
+        dropout_rate = 0.15,            # randomly dropout some pertange of the input tokens
+        bias = True                     # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
     ):
         super().__init__()
         config_args = dict(
-            block_size=8,                 
+            block_size=nb_timesteps,                 
             vocab_size=vocabulary_size,
             nb_tokens_per_timestep=nb_tokens_per_timestep, 
             n_layer=num_layers,                   
