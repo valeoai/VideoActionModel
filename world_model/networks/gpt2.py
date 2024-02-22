903bcd8a7c737345473951216c9b9c6101fe74c8
F-Barto <fbartoc@hotmail.fr>
2024-02-22 19:10:35 +0000

    Remove the usage of GPTConfig + adapted to temp and spatial pos intput

--- world_model/networks/gpt2.py
+++ world_model/networks/gpt2.py
@@ -34,26 +34,31 @@ class LayerNorm(nn.Module):
 
 class CausalSelfAttention(nn.Module):
 
-    def __init__(self, config):
+    def __init__(self, n_embd, n_head, bias, dropout, block_size):
         super().__init__()
-        assert config.n_embd % config.n_head == 0
+        assert n_embd % n_head == 0
+        
+        self.n_head = n_head
+        self.n_embd = n_embd
+        
         # key, query, value projections for all heads, but in a batch
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        self.c_attn = nn.Linear(n_embd, 3 * n_embd, bias=bias)
+        
         # output projection
-        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        self.c_proj = nn.Linear(n_embd, n_embd, bias=bias)
+        
         # regularization
-        self.attn_dropout = nn.Dropout(config.dropout)
-        self.resid_dropout = nn.Dropout(config.dropout)
-        self.n_head = config.n_head
-        self.n_embd = config.n_embd
-        self.dropout = config.dropout
+        self.attn_dropout = nn.Dropout(dropout)
+        self.resid_dropout = nn.Dropout(dropout)
+        
+        self.dropout = dropout
         # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
         self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
         if not self.flash:
             print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
             # causal mask to ensure that attention is only applied to the left in the input sequence
-            self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
-                                        .view(1, 1, config.block_size, config.block_size))
+            self.register_buffer("bias", torch.tril(torch.ones(block_size, block_size))
+                                        .view(1, 1, block_size, block_size))
 
     def forward(self, x):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
@@ -67,7 +72,12 @@ class CausalSelfAttention(nn.Module):
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         if self.flash:
             # efficient attention using Flash Attention CUDA kernels
-            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+            y = torch.nn.functional.scaled_dot_product_attention(
+                q, k, v,
+                attn_mask=None, 
+                dropout_p=self.dropout if self.training else 0,
+                is_causal=True
+            )
         else:
             # manual implementation of attention
             att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
@@ -83,12 +93,12 @@ class CausalSelfAttention(nn.Module):
 
 class MLP(nn.Module):
 
-    def __init__(self, config):
+    def __init__(self, n_embd, bias, dropout):
         super().__init__()
-        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.c_fc    = nn.Linear(n_embd, 4 * n_embd, bias=bias)
         self.gelu    = nn.GELU()
-        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
-        self.dropout = nn.Dropout(config.dropout)
+        self.c_proj  = nn.Linear(4 * n_embd, n_embd, bias=bias)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -99,46 +109,67 @@ class MLP(nn.Module):
 
 class Block(nn.Module):
 
-    def __init__(self, config):
+    def __init__(self, n_embd, n_head, bias, dropout, block_size):
         super().__init__()
-        self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
-        self.attn = CausalSelfAttention(config)
-        self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
-        self.mlp = MLP(config)
+        self.ln_1 = LayerNorm(n_embd, bias=bias)
+        self.attn = CausalSelfAttention(n_embd, n_head, bias, dropout, block_size)
+        self.ln_2 = LayerNorm(n_embd, bias=bias)
+        self.mlp = MLP(n_embd, bias, dropout)
 
     def forward(self, x):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
 
-@dataclass
-class GPTConfig:
-    block_size: int = 8                 # maximum sequence length
-    vocab_size: int = 1104              # vocabulary size = visual_vocab_size + sum(action_vocab_sizes) (e.g. 1104 = 1024 + 50 + 30)
-    nb_tokens_per_timestep: int  = 352  # nb_tokens_per_timestep = #visual-tokens + #action-tokens (e.g. 14*25 + 2)
-    n_layer: int = 12                   # number of transformer blocks
-    n_head: int = 16                    # number of attention heads
-    n_embd: int = 256                   # embedding dimension for the world model    
-    dropout: float = 0.15               # dropout rate
-    bias: bool = True                   # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
-
-class GPT2_Core(nn.Module):
-    def __init__(self, config):
+class GPT2(nn.Module):
+    """
+    GPT2 implementation following the original formulation to be able to load existing pre-trained weights    
+    
+    Args:
+        embedding_dim: embedding dimension for the world model 
+        nb_layers: number of transformer blocks
+        nb_heads: number of attention heads
+        vocabulary_size: total number of vector embeddings in GPT's codebook
+        nb_timesteps: maximum number of timesteps found in one sequence
+        nb_tokens_per_timestep: number of tokens ithi each timestep
+        dropout_rate: dropout rate
+        bias: True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    """
+    def __init__(self, 
+        embedding_dim: int = 256,
+        nb_layers: int = 12,
+        nb_heads: int = 16,
+        vocabulary_size: int = 1104,
+        nb_timesteps: int = 8,
+        nb_tokens_per_timestep: int  = 352,
+        dropout_rate: float = 0.15,
+        bias: bool = True,             
+    ):
+        
         super().__init__()
-        assert config.vocab_size is not None
-        assert config.nb_tokens_per_timestep is not None
-        assert config.block_size is not None
-        self.config = config
+        assert vocabulary_size is not None
+        assert nb_tokens_per_timestep is not None
+        assert nb_timesteps is not None
+        
+        self.nb_timesteps = nb_timesteps
+        self.nb_tokens_per_timestep = nb_tokens_per_timestep
+        
+        self.block_size = nb_timesteps*nb_tokens_per_timestep
+        self.embedding_dim = embedding_dim
+        self.nb_tokens_per_timestep = nb_tokens_per_timestep
 
         self.transformer = nn.ModuleDict(dict(
-            wie = nn.Embedding(config.vocab_size, config.n_embd),               # token embeddings
-            wse = nn.Embedding(config.nb_tokens_per_timestep, config.n_embd),   # spatial position embeddings
-            wte = nn.Embedding(config.block_size, config.n_embd),               # temporal position embeddings
-            drop = nn.Dropout(config.dropout),
-            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-            ln_f = LayerNorm(config.n_embd, bias=config.bias),
+            wie = nn.Embedding(vocabulary_size, embedding_dim),               # token embeddings
+            wse = nn.Embedding(nb_tokens_per_timestep, embedding_dim),   # spatial position embeddings
+            wte = nn.Embedding(nb_timesteps, embedding_dim),               # temporal position embeddings
+            drop = nn.Dropout(dropout_rate),
+            h = nn.ModuleList([
+                Block(embedding_dim, nb_heads, bias, dropout_rate, self.block_size)
+                for _ in range(nb_layers)
+            ]),
+            ln_f = LayerNorm(embedding_dim, bias=bias),
         ))
-        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        self.lm_head = nn.Linear(embedding_dim, vocabulary_size, bias=False)
         # with weight tying when using torch.compile() some warnings get generated:
         # "UserWarning: functional_call was passed multiple values for tied weights.
         # This behavior is deprecated and will be an error in future versions"
@@ -151,7 +182,7 @@ class GPT2_Core(nn.Module):
         # apply special scaled init to the residual projections, per GPT-2 paper
         for pn, p in self.named_parameters():
             if pn.endswith('c_proj.weight'):
-                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * nb_layers))
 
         # report number of parameters
         print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
@@ -177,26 +208,26 @@ class GPT2_Core(nn.Module):
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
-    def forward(self, tokens_sequence, inference=False):
-        # Inputs:
-        #   - token sequence of shape (b x t*(hw+act_sz))
+    def forward(self, token_sequence, temporal_positions, spatial_positions, inference=False):
+        """
+        Args:
+            token_sequence: A tensor of interleaved visual and action tokens.
+            spatial_positions: A tensor indicating the spatial position of each token in the sequence. 
+                example: [0,1,2,3,0,1,2,3]
+            temporal_positions: A tensor indicating the temporal position of each token in the sequence.
+                example: [0,0,0,0,1,1,1,1]
+        """
         
-        device = tokens_sequence.device
-        b, nb_tokens_per_seq = tokens_sequence.size()
-        assert nb_tokens_per_seq + 1 == self.config.block_size * self.config.nb_tokens_per_timestep
+        b, nb_tokens_per_seq = token_sequence.size()
         
         # compute spatio-temporal position embeddings
-        temporal_pos = torch.arange(0, self.config.block_size, dtype=torch.long, device=device)
-        spatial_pos = torch.arange(0, self.config.nb_tokens_per_timestep, dtype=torch.long, device=device)
-        temporal_pos_emb = self.transformer.wte(temporal_pos)                               # (t        x n_embd)
-        spatial_pos_emb = self.transformer.wse(spatial_pos)                                 # (hw+act_sz x n_embd)
-        spatio_temporal_emb = torch.einsum('td,nd->tnd', temporal_pos_emb, spatial_pos_emb) # (t x hw+act_sz x n_embd)
-        spatio_temporal_emb = spatio_temporal_emb.view(-1, self.config.n_embd)[:-1,:]       # ((t*(hw+act_sz)-1) x n_embd)
+        temporal_pos_emb = self.transformer.wte(temporal_positions)
+        spatial_pos_emb = self.transformer.wse(spatial_positions)
         
         # compute image and action embeddings
-        tok_emb = self.transformer.wie(tokens_sequence)                                     # token embeddings of shape (b x t x (hw+act_sz) x n_embd)
-        emb_in = tok_emb + spatio_temporal_emb                                              # (b x t x (hw+act_sz) x n_embd)
-        emb_in = emb_in.view(b, -1, self.config.n_embd)                                     # (b x t*(hw+act_sz) x n_embd)
+        tok_emb = self.transformer.wie(token_sequence)                                      # token embeddings of shape (b x t x (hw+act_sz) x n_embd)
+        emb_in = tok_emb + temporal_pos_emb + spatial_pos_emb                               # (b x t x (hw+act_sz) x n_embd)
+        emb_in = emb_in.view(b, -1, self.embedding_dim)                                     # (b x t*(hw+act_sz) x n_embd)
         
         # forward world embeddings to the transformer
         x = self.transformer.drop(emb_in)
@@ -208,37 +239,5 @@ class GPT2_Core(nn.Module):
             img_logits = self.lm_head(emb_out)
         else:
             img_logits = self.lm_head(emb_out[:, [-1], :]) # note: using list [-1] to preserve the time dim
+            
         return img_logits
-
-class GPT2(nn.Module):
-    
-    def __init__(
-        self, 
-        embedding_dim,                  # embedding dimension for the world model
-        num_heads,                      # number of attention heads
-        num_layers,                     # number of transformer blocks
-        vocabulary_size,                # vocabulary size = visual_vocab_size + sum(action_vocab_sizes)
-        nb_timesteps,                   # maximum sequence length
-        nb_tokens_per_timestep,         # nb_tokens_per_timestep = #visual-tokens + #action-tokens
-        dropout_rate = 0.15,            # randomly dropout some pertange of the input tokens
-        bias = True                     # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
-    ):
-        super().__init__()
-        config_args = dict(
-            block_size=nb_timesteps,                 
-            vocab_size=vocabulary_size,
-            nb_tokens_per_timestep=nb_tokens_per_timestep, 
-            n_layer=num_layers,                   
-            n_head=num_heads,                    
-            n_embd=embedding_dim,
-            dropout=dropout_rate,
-            bias=bias
-        )
-        configs = GPTConfig(**config_args)
-        self.network = GPT2_Core(configs)
-        
-    def forward(self, tokens_sequence):
-        return self.network.forward(tokens_sequence)
-    
-    def inference_forward(self, tokens_sequence):
-        return self.network.forward(tokens_sequence, inference=True)
