43a4bbdf03b593884c6f787a0d5ac701b134825c
F-Barto <fbartoc@hotmail.fr>
2024-04-25 09:04:53 +0000

    adastras torch 2.4.0 dev for flash attention

--- scripts/activate_world_model_env_adastra.sh
+++ scripts/activate_world_model_env_adastra.sh
@@ -1,5 +1,5 @@
 module purge
-source /lus/work/CT10/cin4181/SHARED/shared_python_envs/wm_world_model_env/bin/activate # Activate custom env
+source /lus/work/CT10/cin4181/SHARED/shared_python_envs/wm_world_model_env_torch2.4.0dev/bin/activate # Activate custom env
 module load craype-accel-amd-gfx90a craype-x86-trento # Compiler ?
 module load PrgEnv-cray # devkit ?
 module load amd-mixed # AMD hardware
--- scripts/create_world_model_env_adastra.sh
+++ scripts/create_world_model_env_adastra.sh
@@ -5,7 +5,7 @@ set -eu
 module purge
 module load cray-python
 
-env_name=wm_world_model_env
+env_name=wm_world_model_env_2.4.0dev
 
 # Install  virtualenv
 python3 -m pip install --user --upgrade pip
@@ -25,7 +25,7 @@ source ./activate_world_model_env.sh
 
 # install all necessary python packages
 python3 -m pip install --upgrade pip --no-cache-dir
-pip3 install torch==2.2.0 torchvision==0.17.0 --index-url https://download.pytorch.org/whl/rocm5.6 --no-cache-dir
+pip3 install --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/rocm5.7 --no-cache-dir
 pip3 install einops omegaconf click scipy --no-cache-dir
 pip3 install lightning==2.2.0 --no-cache-dir
 pip3 install torchmetrics==0.11.4 --no-cache-dir
