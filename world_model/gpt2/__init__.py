764c32889e33be989d171bc253ee5c3a084145b3
Elias Ramzi <elias.ramzi@gmail.com>
2025-01-14 17:30:43 +0000

    starting config files

--- world_model/gpt2/__init__.py
+++ world_model/gpt2/__init__.py
@@ -1,9 +1,15 @@
+from world_model.gpt2.joint_model import JointModel
+from world_model.gpt2.mup_action_expert import MupActionExpert
 from world_model.gpt2.mup_gpt2 import MupGPT2
 from world_model.gpt2.next_token_predictor import NextTokenPredictor
+from world_model.gpt2.vai0rbis import Vai0rbis
 from world_model.gpt2.warmup_stable_drop import WarmupStableDrop
 
 __all__ = [
+    "JointModel",
+    "MupActionExpert",
     "MupGPT2",
     "NextTokenPredictor",
+    "Vai0rbis",
     "WarmupStableDrop",
 ]
