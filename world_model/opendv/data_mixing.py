1a533d87e620a0429fb87f0a082c72a2c87e3c7b
F-Barto <fbartoc@hotmail.fr>
2025-01-17 23:06:36 +0000

    sum of ratios must be <= 1

--- world_model/opendv/data_mixing.py
+++ world_model/opendv/data_mixing.py
@@ -31,6 +31,7 @@ def mix_datasets(
     seed: int = 0,
 ) -> ConcatDataset:
     assert len(datasets) == len(ratios), "The number of datasets and ratios must be the same"
+    assert sum(ratios) <= 1., f"the sum of dataset ratios ({sum(ratios)}) is bigger than 1 ! "
     assert [r >= 0 for r in ratios], "Ratios must be positive"
     new_dataset_size = [int(r * total_number_of_samples) for r in ratios]
 
