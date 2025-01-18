f650386e9ecf9355755309580d7834766cece335
Elias Ramzi <elias.ramzi@gmail.com>
2025-01-18 15:24:49 +0100

    introduced import issue (#32)

--- world_model/utils/__init__.py
+++ world_model/utils/__init__.py
@@ -2,7 +2,7 @@ from world_model.utils.cmd_line_logging import RankedLogger
 from world_model.utils.expand_path import expand_path
 from world_model.utils.hyperparam_logging import log_hyperparameters
 from world_model.utils.info_printing import print_config_tree
-from world_model.utils.instantiators import instantiate_callbacks, instantiate_loggers, instantiate_samplers
+from world_model.utils.instantiators import instantiate_callbacks, instantiate_loggers
 from world_model.utils.plot_utils import plot_multiple_images
 from world_model.utils.task_utils import extras, get_metric_value, task_wrapper
 
@@ -13,7 +13,6 @@ __all__ = [
     "print_config_tree",
     "instantiate_callbacks",
     "instantiate_loggers",
-    "instantiate_samplers",
     "plot_multiple_images",
     "extras",
     "get_metric_value",
